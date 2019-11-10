614963209130dff8c781ee8fd60f59f6646cbe83
dong <dxiesjz@163.com>
2019-11-10 15:14:40 +0800

    首页模块

--- src/main/java/com/ms/controller/DeskManagerController.java
+++ src/main/java/com/ms/controller/DeskManagerController.java
@@ -5,7 +5,9 @@ import com.ms.entity.Leavebill;
 import com.ms.entity.User;
 import com.ms.response.DataGridView;
 import com.ms.service.ILeavebillService;
+import com.ms.service.IWorkFlowService;
 import com.ms.utils.WebUtils;
+import com.ms.vo.WorkFlowVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,9 @@ import java.util.List;
 public class DeskManagerController {
     @Autowired
     private ILeavebillService leavebillService;
+
+    @Autowired
+    private IWorkFlowService workFlowService;
     /**
      * 加载我的申请任务
      * @return
@@ -39,5 +44,23 @@ public class DeskManagerController {
         queryWrapper.orderByDesc("id");
         List<Leavebill> list = leavebillService.list(queryWrapper);
         return new DataGridView(list);
+    }
+    //加载我的待办任务
+    @RequestMapping("loadAllTask")
+    public DataGridView loadAllTask(){
+        WorkFlowVo workFlowVo = new WorkFlowVo();
+        workFlowVo.setPage(1);
+        workFlowVo.setLimit(5);
+        return this.workFlowService.loadCurrentUserTask(workFlowVo);
+    }
+    /**
+     * 查看经办任务
+     */
+    @RequestMapping("loadHistoyTask")
+    public DataGridView loadHistoyTask(){
+        WorkFlowVo workFlowVo = new WorkFlowVo();
+        workFlowVo.setPage(1);
+        workFlowVo.setLimit(5);
+        return this.workFlowService.queryCurrentUserHistoryTask(workFlowVo);
     }
 }
